7a45df5bd73f3e054eaf41fc7fc9b70f745d62c9
Snychng <snychng@gmail.com>
2025-04-26 12:20:24 +0800

    自动下载模型

---
+++ main/downloadmodels.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+model = YOLO('models/yolo11n.pt')
+model = YOLO('models/yolo11s.pt')
+model = YOLO('models/yolo11m.pt')
+model = YOLO('models/yolo11l.pt')
+model = YOLO('models/yolo11x.pt')
